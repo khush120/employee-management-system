b75de1147872b9c4502bf629f7f05777a3b5f267
khushbu <khushyadv0303@gmail.com>
2026-07-29 16:36:31 +0530

    Updated EmployeeList

--- frontend/src/pages/EmployeeList.jsx
+++ frontend/src/pages/EmployeeList.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 function EmployeeList() {
   const {
     employees,
+
     setEmployees,
     setEditingEmployee,
   } = useContext(EmployeeContext);
